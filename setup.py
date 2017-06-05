8a0d26b83a2ac794c20e0bbf7cf46043a944382b
Jack Stephenson <jack@bancast.net>
2017-06-05 04:34:16 +0100

    Add requirements

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+try:
+    with open('README.md') as f:
+        long_description = f.read()
+except:
+    long_description = ''
+
+setup(
+    name='latex2sympy3',
+    version='0.1.0',
+    description='Sympy generator from LaTeX expressions.',
+    long_description=long_description,
+    maintainer='jack',
+    maintainer_email='jack@bancast.net',
+    url='https://github.com/jackatbancast/latex2sympy',
+    packages=['latex2sympy'],
+    license='MIT',
+    requires=['antlr4-python3-runtime', 'sympy']
+)
